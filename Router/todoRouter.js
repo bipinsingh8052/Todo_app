8f1ed7ed6ceb5daa84595f1d88aa47fa6725f849
Bipin Singh <bipinsingh8052@gmail.com>
2025-01-29 01:56:19 +0530

    insert the data in todo app

--- Router/todoRouter.js
+++ Router/todoRouter.js
@@ -3,6 +3,8 @@ const route=express.Router();
 const todocontroller =require("../controller/todocontroller")
 route.get("/home",todocontroller.homePage)
 route.post("/insert",todocontroller.InserPage)
+route.delete("/delete",todocontroller.deletePage)
+route.put("/update",todocontroller.updatepage)
 
 
 module.exports=route;
--- controller/todocontroller.js
+++ controller/todocontroller.js
@@ -1,12 +1,27 @@
 const todoMongoose =require("../Models/TodoModel")
-const homePage=(req,res)=>{
-    res.send("home page");
+const homePage=async(req,res)=>{
+    let data =await todoMongoose.find();
+    res.send(data);
+    // res.send("home page");
 }
-const InserPage=(req,res)=>{
-    res.send("INsert page")
-}
+const InserPage=async(req,res)=>{
+    console.log(req.body);
+    const {description, title}=req.body;
+    const data =await todoMongoose.create({description,title
+    })
 
+    res.send(data);
+    // res.send("INsert page")
+}
+const deletePage =(req,res)=>{
+    req.send("Delete page")
+}
+const updatepage=(req,res)=>{
+    req.send("update page")
+}
 module.exports={
     homePage,
-    InserPage
+    InserPage,
+    deletePage,
+    updatepage
 }
